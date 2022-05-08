228c71d6408df75e63ec507350c33ead7347f4dd
Peter Upfold <pgithub@upfold.org.uk>
2022-05-08 15:11:53 +0100

    Prints first username

--- main.cpp
+++ main.cpp
@@ -13,11 +13,20 @@ HANDLE serviceStopEvent = INVALID_HANDLE_VALUE;
 int _tmain(int argc, TCHAR* argv[]) {
 
 	ULONG logonSessionCount = 0;
-	LUID logonSessionList = { 0 };
-	PLUID logonSessionListPtr = &logonSessionList;
+	PLUID logonSessionListPtr = nullptr;
+	PSECURITY_LOGON_SESSION_DATA logonSessionData = nullptr;
 
 	if (STATUS_SUCCESS == LsaEnumerateLogonSessions(&logonSessionCount, &logonSessionListPtr)) {
-		printf("logon session count: %d", logonSessionCount);
+		printf("logon session count: %d\n", logonSessionCount);
+
+		if (STATUS_SUCCESS == LsaGetLogonSessionData(logonSessionListPtr, &logonSessionData)) {
+			wprintf(L"username: %s\n", logonSessionData->UserName.Buffer);
+		}
+		else {
+			printf("LsaGetLogonSession data fail\n");
+		}
+
+		LsaFreeReturnBuffer(logonSessionListPtr);
 	}
 	else {
 		printf("FAIL: %d", GetLastError());
