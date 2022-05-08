0f88689bd5e07f085233cca3df30bc9b26560ac4
Peter Upfold <pgithub@upfold.org.uk>
2022-05-08 15:38:18 +0100

    First succeeds, second access denied, then fail

--- main.cpp
+++ main.cpp
@@ -13,24 +13,39 @@ HANDLE serviceStopEvent = INVALID_HANDLE_VALUE;
 int _tmain(int argc, TCHAR* argv[]) {
 
 	ULONG logonSessionCount = 0;
+	ULONG originalSessionCount = 0;
 	PLUID logonSessionListPtr = nullptr;
 	PSECURITY_LOGON_SESSION_DATA logonSessionData = nullptr;
 
-	if (STATUS_SUCCESS == LsaEnumerateLogonSessions(&logonSessionCount, &logonSessionListPtr)) {
-		printf("logon session count: %d\n", logonSessionCount);
+	PLUID nextLogonSessionID = nullptr;
 
-		if (STATUS_SUCCESS == LsaGetLogonSessionData(logonSessionListPtr, &logonSessionData)) {
+	NTSTATUS result = LsaEnumerateLogonSessions(&logonSessionCount, &logonSessionListPtr);
+	originalSessionCount = logonSessionCount;
+
+	if (STATUS_SUCCESS != result) {
+		printf("Faield to get logon session count: 0x%x", result);
+		return result;
+	}
+
+	printf("logon session count: %d\n", logonSessionCount);
+
+	do {
+		NTSTATUS result = LsaGetLogonSessionData(logonSessionListPtr, &logonSessionData);
+
+		if (STATUS_SUCCESS == result) {
 			wprintf(L"username: %s\n", logonSessionData->UserName.Buffer);
 		}
 		else {
-			printf("LsaGetLogonSession data fail\n");
+			printf("LsaGetLogonSession data fail: 0x%x\n", result);
 		}
 
-		LsaFreeReturnBuffer(logonSessionListPtr);
+		logonSessionListPtr += sizeof(PLUID);
+		originalSessionCount--;
+		printf("logonSessionCount now: %lld\n", originalSessionCount);
 	}
-	else {
-		printf("FAIL: %d", GetLastError());
-	}
+	while (originalSessionCount > 0);
+
+	LsaFreeReturnBuffer(logonSessionListPtr);
 
 	return 0;
 }
