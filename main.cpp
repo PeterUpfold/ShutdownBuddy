2b0ec276a524cc630cd4493824d41e562ed1d2f9
Peter Upfold <pgithub@upfold.org.uk>
2022-05-08 15:03:59 +0100

    LsaEnumerateLogonSessions being called

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
+#include <stdio.h>
 #include <Windows.h>
 #include <tchar.h>
+#include <NTSecAPI.h>
+#include <ntstatus.h>
 #include "main.h"
+#pragma comment(lib, "secur32")
 
 SERVICE_STATUS serviceStatus = { 0 };
 SERVICE_STATUS_HANDLE statusHandle = nullptr;
 HANDLE serviceStopEvent = INVALID_HANDLE_VALUE;
 
+int _tmain(int argc, TCHAR* argv[]) {
+
+	ULONG logonSessionCount = 0;
+	LUID logonSessionList = { 0 };
+	PLUID logonSessionListPtr = &logonSessionList;
+
+	if (STATUS_SUCCESS == LsaEnumerateLogonSessions(&logonSessionCount, &logonSessionListPtr)) {
+		printf("logon session count: %d", logonSessionCount);
+	}
+	else {
+		printf("FAIL: %d", GetLastError());
+	}
+
+	return 0;
+}
+
+void WINAPI ServiceMain(DWORD argc, LPTSTR* argv) {
+
+}
