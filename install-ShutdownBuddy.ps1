50fec2b7b7556ac54a5372383019b1edca1d5552
Peter Upfold <pgithub@upfold.org.uk>
2023-02-14 14:59:04 +0000

    Add -Force to installer to create some parent paths

--- install-ShutdownBuddy.ps1
+++ install-ShutdownBuddy.ps1
@@ -16,25 +16,25 @@ $sdbPath = "C:\Program Files\upfold.org.uk\ShutdownBuddy"
 $regRoot = "HKLM:\SOFTWARE\upfold.org.uk\ShutdownBuddy"
 
 try {
-    New-Item -ItemType Directory $sdbPath
+    New-Item -ItemType Directory $sdbPath -Force
 }
 catch {
     Write-Host "$sdbPath already exists, or no permissions?"
 }
 
 if (-not (Test-Path $regRoot)) {
-    New-Item $regRoot
+    New-Item $regRoot -Force
 }
 
 if ($DebugLog) {
-    Set-ItemProperty -Path $regRoot -Name "DebugLog" -Value 1
+    Set-ItemProperty -Path $regRoot -Name "DebugLog" -Value 1 -Force
 }
 else {
-    Set-ItemProperty -Path $regRoot -Name "DebugLog" -Value 0
+    Set-ItemProperty -Path $regRoot -Name "DebugLog" -Value 0 -Force
 }
 
-Set-ItemProperty -Path $regRoot -Name "EvaluationIntervalSeconds" -Value $EvaluationIntervalSeconds
-Set-ItemProperty -Path $regRoot -Name "ShutdownAfterIdleForSeconds" -Value $ShutdownAfterIdleForSeconds
+Set-ItemProperty -Path $regRoot -Name "EvaluationIntervalSeconds" -Value $EvaluationIntervalSeconds -Force
+Set-ItemProperty -Path $regRoot -Name "ShutdownAfterIdleForSeconds" -Value $ShutdownAfterIdleForSeconds -Force
 
 # copy files
 Copy-Item ShutdownBuddy.exe "$sdbPath" -Force
